8c189cbb691ef84e336257f4bf9fe7e0778888c5
Pepakayala Veera Ganesh Kumar <83122289+ganesh5124@users.noreply.github.com>
2024-11-14 01:03:34 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+
+# AWS cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
+
+# Kubectl cli
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+
+kubectl version --client
+
+# Eks ctl
+sudo apt update
+sudo apt install -y curl
+curl -s --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
